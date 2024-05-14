ebd96f877253027928bc0a3b9aed2c356e798dc6
Fred78290 <frederic.boltz@gmail.com>
2024-05-14 11:50:40 +0200

    TYPO: rename variable

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,9 +1,9 @@
-DASHABOARD_FILES=(_1710_project_dns_panel_group.py _1721_dns_zones_panel.py _1722_dns_reversedns_panel.py)
+DASHBOARD_FILES=(_1710_project_dns_panel_group.py _1721_dns_zones_panel.py _1722_dns_reversedns_panel.py)
 
 function install_designatedashboard {
     setup_develop ${DESIGNATEDASHBOARD_DIR}
 
-    for panel in ${DASHABOARD_FILES[@]}; do
+    for panel in ${DASHBOARD_FILES[@]}; do
         ln -fs $DESIGNATEDASHBOARD_DIR/designatedashboard/enabled/$panel \
 			$HORIZON_DIR/openstack_dashboard/local/enabled/$panel
     done
@@ -37,7 +37,7 @@ if is_service_enabled horizon && is_service_enabled designate; then
     if [[ "$1" == "clean" ]]; then
         # Remove state and transient data
         # Remember clean.sh first calls unstack.sh
-		for panel in ${DASHABOARD_FILES[@]}; do
+		for panel in ${DASHBOARD_FILES[@]}; do
 			rm $HORIZON_DIR/openstack_dashboard/local/enabled/$panel
 		done
     fi
